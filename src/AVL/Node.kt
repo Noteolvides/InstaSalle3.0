306c5a510246653d5ad4625f40b627b6f270793b
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-04-06 16:59:08 +0200

    Mas Inicializacion de clases

--- src/AVL/Node.kt
+++ src/AVL/Node.kt
@@ -1,4 +1,15 @@
 package AVL
 
-class Node {
+class Node<T>(data : T,parent: Node<T>) {
+    var parent: Node<T>? = null
+    var data: T? = null
+    var rightChild : Node<T>? = null
+    var leftChild : Node<T>? = null
+
+    init {
+        this.data = data;
+        this.parent = parent;
+    }
+
+
 }
