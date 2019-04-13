9417fe00e53c6c1a30a179e102b5690ebecdb787
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-04-13 17:51:07 +0200

    #8 feat: Added List class

--- src/main/java/Graphs/List.java
+++ src/main/java/Graphs/List.java
@@ -2,32 +2,29 @@ package Graphs;
 
 public class List<E> {
     // TODO: 2019-04-12 Hacer lista de c de apuntes de primero 
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
 
     public List() {
-        
+        this.head = new Node<E>(null);
+        this.tail = head;
+        this.size = 0;
     }
 
     public void add(E element) {
-
-    }
-
-    public void remove(int index) {
-
+        tail = tail.next = new Node<E>(element);
     }
 
     public void remove(E element) {
 
     }
 
-    public void get(int index) {
-
-    }
-
-    public void get(E element) {
-
+    public E get(E element) {
+        return element;
     }
 
     public int size(){
-        return size;
+         return size;
     }
 }
