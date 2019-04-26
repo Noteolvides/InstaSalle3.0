fd93e576a7a6a1c6f3185f5b0e17764cbc2daf18
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-04-26 20:12:03 +0200

    #13 feat: Added Data Classes

---
+++ src/main/java/Data/Post.java
@@ -0,0 +1,10 @@
+package Data;
+
+public class Post {
+    public int id;
+    public User[] likedBy;
+    public String publishedWhen;
+    public String publishedBy;
+    public Long[] location;
+    public String[] hashtags;
+}
---
+++ src/main/java/Data/User.java
@@ -0,0 +1,7 @@
+package Data;
+
+public class User {
+    public String username;
+    public String creation;
+    public User[] toFollow;
+}
