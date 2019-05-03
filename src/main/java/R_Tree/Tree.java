e0e255539c7095f0b08709dccff8d1e09d85e785
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-03 15:26:56 +0200

    #14 Rtree class

--- src/main/java/R_Tree/Tree.java
+++ src/main/java/R_Tree/Tree.java
@@ -1,4 +1,18 @@
 package R_Tree;
 
-public class Tree {
+public class Tree<T extends Comparable> {
+	final static int BTREEDIMENSION = 2;
+	Region<T> root;
+
+
+	public Tree() {
+		this.root = new Region<T>(BTREEDIMENSION);
+	}
+
+
+	//Todo Insercion
+
+	//Todo Busqueda
+
+	//Todo Eliminacion
 }
