4711a7d2fbd6b3b49a54ec42ec107f59d3bf0e9e
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-03 15:19:04 +0200

    #14 Region class

--- src/main/java/R_Tree/Region.java
+++ src/main/java/R_Tree/Region.java
@@ -1,4 +1,26 @@
 package R_Tree;
 
-public class Region {
+import java.lang.reflect.Array;
+
+public class Region <T extends Comparable> {
+	Region subRegions [];
+	Boolean isLeaf;
+	T pointsLeaf[];
+
+	public Region(T data) {
+		isLeaf = false;
+		subRegions = new Region[2];
+	}
+
+	public Region(T data,int dimension,Class<T> c){
+		this.isLeaf = true;
+		@SuppressWarnings("unchecked")
+		final T[] a = (T[]) Array.newInstance(c, dimension);
+	}
+
+	public Region(T data,int dimension){
+		this.isLeaf = false;
+		this.data = data;
+		subRegions = new Region[dimension];
+	}
 }
