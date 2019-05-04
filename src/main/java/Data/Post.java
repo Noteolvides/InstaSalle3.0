9cfdb2891b72b762c796f381fe1902a604ee1265
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-04 15:39:27 +0200

    #13 fix: Type changed to reduce JSON complexity

--- src/main/java/Data/Post.java
+++ src/main/java/Data/Post.java
@@ -2,13 +2,13 @@ package Data;
 
 public class Post {
     public int id;
-    public User[] likedBy;
+    public String[] likedBy;
     public String publishedWhen;
     public String publishedBy;
     public Long[] location;
     public String[] hashtags;
 
-    public Post(int id, User[] likedBy, String publishedWhen, String publishedBy, Long[] location, String[] hashtags) {
+    public Post(int id, String[] likedBy, String publishedWhen, String publishedBy, Long[] location, String[] hashtags) {
         this.id = id;
         this.likedBy = likedBy;
         this.publishedWhen = publishedWhen;
