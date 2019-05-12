f30c32045df88a7e40fb32fd2fd09dd5d2b23eba
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-12 17:09:54 +0200

    #15 feat: Added menu of imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,49 @@
-import Graphs.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph();
-        graph.insertVertex(new Vertex("n", "6"));
-        graph.insertVertex(new Vertex("neil", "20/4/2019"));
-        graph.insertVertex(new Vertex("alex", "13/5/2019"));
-        graph.insertEdge(new Vertex("neil", "20/4/2019"),new Vertex("alex", "13/5/2019"));
-        graph.insertEdge(new Vertex("n", "6"), new Vertex("alex", "13/5/2019"));
-        graph.insertEdge( new Vertex("alex", "13/5/2019"), new Vertex("n", "6"));
-        graph.removeEdge(new Vertex("n", "6"), new Vertex("alex", "13/5/2019"));
-        graph.removeVertex(new Vertex("n", "6"));
+        String userspath;
+        String postspath;
+        String triepath;
+        String rtreepath;
+        String avltreepath;
+        String hashpath;
+        String graphpath;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Importació de fitxers\n" +
+                "1. Exportació de fitxers en format usuaris i post" +
+                "2. Exportació de fitxers en format totes les estructures" +
+                "> ");
+        int option = sc.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a usuaris\n");
+                System.out.println("> ");
+                userspath = sc.next();
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a posts\n");
+                System.out.println("> ");
+                postspath = sc.next();
+                break;
+            case 2:
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a Tries\n");
+                System.out.println("> ");
+                triepath = sc.next();
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a R-Trees\n");
+                System.out.println("> ");
+                rtreepath = sc.next();
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a AVL Tree\n");
+                System.out.println("> ");
+                avltreepath = sc.next();
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a Hash Table\n");
+                System.out.println("> ");
+                hashpath = sc.next();
+                System.out.println("Especifiqui ruta del fitxer a importar corresponent a Graph\n");
+                System.out.println("> ");
+                graphpath = sc.next();
+                break;
+        }
+
+        }
     }
 }
