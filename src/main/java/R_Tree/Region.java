7e2c26464b4a17bb8443ce5d5206772bcf65bba3
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-14 20:13:58 +0200

    #14 feat: Added location update to add

--- src/main/java/R_Tree/Region.java
+++ src/main/java/R_Tree/Region.java
@@ -25,6 +25,8 @@ public class Region {
 		this.isLeaf = true;
 		pointsLeaf = new Post[2];
 		pointsLeaf[0] = post;
+		this.min = new Point(post.location[0],post.location[1]);
+		this.max = new Point(post.location[0],post.location[1]);
 	}
 
 	public Region(int dimension){
@@ -38,6 +40,19 @@ public class Region {
 				pointsLeaf[0] = (Post) o;
 			} else {
 				pointsLeaf[1] = (Post) o;
+				this.isfull = true;
+			}
+			if (((Post) o).location[0] < min.x) {
+				min.x = ((Post) o).location[0];
+			}
+			if (((Post) o).location[1] < min.y) {
+				min.y = ((Post) o).location[1];
+			}
+			if (((Post) o).location[0] > max.x) {
+				max.x = ((Post) o).location[0];
+			}
+			if (((Post) o).location[1] > max.y) {
+				max.y = ((Post) o).location[1];
 			}
 		} else {
 			Region newSubRegion = new Region((Post) o);
