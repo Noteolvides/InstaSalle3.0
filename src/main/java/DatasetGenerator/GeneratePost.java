2c4099fd4fd3277cae8cb559d6895c2286baf0a5
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-15 14:48:28 +0200

    #13 fix: Changes of types

--- src/main/java/DatasetGenerator/GeneratePost.java
+++ src/main/java/DatasetGenerator/GeneratePost.java
@@ -30,15 +30,15 @@ public class GeneratePost {
             for (int j = 0; j < arraysize; j++){
                 likedBy[j] = createName();
             }
-            Long[] location = new Long[2];
-            location[0] = (long) rdm.nextInt((1200) + 1);
-            location[1] = (long) rdm.nextInt((700) + 1);
+            Double[] location = new Double[2];
+            location[0] = (double) rdm.nextInt((1200) + 1);
+            location[1] = (double) rdm.nextInt((700) + 1);
             arraysize = rdm.nextInt(numUsers);
             String[] hashtags = new String[arraysize];
             for (int j = 0; j < arraysize; j++){
                 hashtags[j] = createName();
             }
-            posts.add(new Post(i, likedBy, new Date().toString(), createName(), (Long[]) location, hashtags));
+            posts.add(new Post(i, likedBy, rdm.nextInt(), createName(), (Double[]) location, hashtags));
         }
 
         for (int i = 0; i < posts.size(); i++) {
