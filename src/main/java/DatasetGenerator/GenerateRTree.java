986a346c4597409c3acd73d88807ab9f0228dd0f
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-21 19:54:02 +0200

    #13 feat: Added RTree generator

---
+++ src/main/java/DatasetGenerator/GenerateRTree.java
@@ -0,0 +1,48 @@
+package DatasetGenerator;
+
+import Data.Post;
+import R_Tree.Tree;
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GenerateRTree {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of posts: ");
+        int numPosts = sc.nextInt();
+        System.out.println("Number of users: ");
+        int numUsers = sc.nextInt();
+
+        Tree rTree = new Tree();
+        Random rdm = new Random();
+
+        for (int i = 0; i < numPosts; i++) {
+            int arraysize = rdm.nextInt(numUsers);
+            String[] likedBy = new String[arraysize];
+            for (int j = 0; j < arraysize; j++){
+                likedBy[j] = Utils.createName();
+            }
+            Double[] location = new Double[2];
+            location[0] = (double) rdm.nextInt((1200) + 1);
+            location[1] = (double) rdm.nextInt((700) + 1);
+            arraysize = rdm.nextInt(numUsers);
+            String[] hashtags = new String[arraysize];
+            for (int j = 0; j < arraysize; j++){
+                hashtags[j] = Utils.createName();
+            }
+            Post post = new Post(i, likedBy, rdm.nextLong(), Utils.createName(), (Double[]) location, hashtags);
+            //rTree.insertion(post);
+        }
+
+        Gson gson = new Gson();
+        String jsonString;
+        jsonString = gson.toJson(rTree);
+        FileWriter fw = new FileWriter("datasets/generated/datasetRTree" + numPosts + ".json");
+        fw.write(jsonString);
+        fw.close();
+    }
+}
