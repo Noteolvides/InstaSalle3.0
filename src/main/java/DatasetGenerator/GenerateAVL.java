1487bd7cd0f5eea9451dbd3f456668191131c068
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-22 09:16:31 +0200

    #13 fix: Decommented parts

--- src/main/java/DatasetGenerator/GenerateAVL.java
+++ src/main/java/DatasetGenerator/GenerateAVL.java
@@ -17,7 +17,7 @@ public class GenerateAVL {
         System.out.println("Number of users: ");
         int numUsers = sc.nextInt();
 
-        //AVLTree<Post> avltree = new AVLTree<>();
+        AVLTree<Post> avltree = new AVLTree<>();
         for (int i = 0; i < numPosts; i++) {
             Random rdm = new Random();
             int arraysize = rdm.nextInt(numUsers);
@@ -33,14 +33,14 @@ public class GenerateAVL {
             for (int j = 0; j < arraysize; j++){
                 hashtags[j] = Utils.createName();
             }
-            //avltree.insert(new Post());
+            avltree.insert(new Post());
         }
 
         Gson gson = new Gson();
         String jsonString;
-        //jsonString = gson.toJson(avltree);
+        jsonString = gson.toJson(avltree);
         FileWriter fw = new FileWriter("datasets/generated/datasetAVL" + numPosts + ".json");
-        //fw.write(jsonString);
+        fw.write(jsonString);
         fw.close();
     }
 }
