7d18015e82076afc408d3afdfd7ab0b17ec212b6
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-22 15:10:09 +0200

    #13 fix: Not random time of generation of random name

--- src/main/java/DatasetGenerator/Utils.java
+++ src/main/java/DatasetGenerator/Utils.java
@@ -5,9 +5,16 @@ import java.util.Random;
 public class Utils {
     public static String createName() {
         Random rdm = new Random();
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         String name = "";
         for (int i = 0; i < rdm.nextInt(10); i++) {
             name += (char) (rdm.nextInt(26) + 'a');
+        }
+        if (name.equals("")) {
+            name += alphabet[rdm.nextInt(26)];
+            name += alphabet[rdm.nextInt(26)];
+            name += alphabet[rdm.nextInt(26)];
+            name += alphabet[rdm.nextInt(26)];
         }
         return name;
     }
