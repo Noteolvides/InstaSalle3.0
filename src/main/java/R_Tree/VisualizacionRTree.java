998117ced7c6507201f9f9b0763cefed1e759ef0
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-06-18 01:49:09 +0200

    Animation cool

--- src/main/java/R_Tree/VisualizacionRTree.java
+++ src/main/java/R_Tree/VisualizacionRTree.java
@@ -18,7 +18,7 @@ public class VisualizacionRTree extends PApplet {
     public static void main(String[] args) {
         Random rd = new Random();
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 1000; i++) {
             int x = rd.nextInt(RTree.WIDTH_SCRREN);
             int y = rd.nextInt(RTree.HEIGHT_SCREEN);
             try{
@@ -30,7 +30,7 @@ public class VisualizacionRTree extends PApplet {
         }
 
         PApplet.main("R_Tree.VisualizacionRTree", args);
-        System.out.println("12");
+        //System.out.println("12");
     }
 
 
@@ -40,11 +40,11 @@ public class VisualizacionRTree extends PApplet {
 
     public void setup() {
         background(51);
-        preOrder(tree.root,0);
+        preOrder(tree.root, 0);
     }
 
 
-    public void draw(){/*
+    public void draw() {/*
         int x;
         int y;
         System.out.println("introduce X");
@@ -55,9 +55,19 @@ public class VisualizacionRTree extends PApplet {
         LEVEL = 0;
         checkHeigh(tree.root);
         preOrder(tree.root,0);*/
+        delay(1000);
+        int x = rd.nextInt(RTree.WIDTH_SCRREN);
+        int y = rd.nextInt(RTree.HEIGHT_SCREEN);
+        try {
+            tree.insert(new Point(x, y));
+            preOrder(tree.root, 0);
+        } catch (Exception e) {
+        }
     }
 
-    private void preOrder(NodeRTree node,int level) {
+
+
+    private void preOrder(NodeRTree node, int level) {
         int x;
         int y;
 
@@ -93,8 +103,8 @@ public class VisualizacionRTree extends PApplet {
                     rect(minPoint.x, minPoint.y, maxPoint.x - minPoint.x, maxPoint.y - minPoint.y);
                     textAlign(CENTER);
                     textSize(10);
-                    text(Character.toString((char) (65+i)) +level,minPoint.x+(maxPoint.x-minPoint.x)/2,minPoint.y+(maxPoint.y-minPoint.y)/2);
-                    preOrder(node.regions[i],level+1);
+                    text(Character.toString((char) (65 + i)) + level, minPoint.x + (maxPoint.x - minPoint.x) / 2, minPoint.y + (maxPoint.y - minPoint.y) / 2);
+                    preOrder(node.regions[i], level + 1);
                 }
             }
         }
